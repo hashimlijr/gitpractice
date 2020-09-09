24d93b51c78f887c0dabb6c7270503ff0e2e3c6f
Mahammad Verdiyev <devmahammad@gmail.com>
2020-09-09 18:04:44 +0400

    added new methods to the project

--- MathCalculations/src/Main.java
+++ MathCalculations/src/Main.java
@@ -1,10 +1,11 @@
-import com.util.*;
+import com.util.MyMath;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
+		int[] array = {2,2,2,2,4,4,4,4,4,4,6,7,9,12,32,11};
+		System.out.println(MyMath.count(4, array));
+		System.out.println(MyMath.countMin(array));
 	}
 
 }
--- MathCalculations/src/com/util/MyMath.java
+++ MathCalculations/src/com/util/MyMath.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class MyMath 
 {
-	static int gcd(int a, int b) 
+	public static int gcd(int a, int b) 
 	{
 		if(a == 0) return b;
         if(b == 0) return a;
@@ -25,7 +25,7 @@ public class MyMath
 	}
 	
 	
-	static int gcd(int[] elements)
+	public static int gcd(int[] elements)
 	{
 		int result = 0;
 
@@ -39,7 +39,7 @@ public class MyMath
 	}
 	
 	
-	static long sum(int[] elements)
+	public static long sum(int[] elements)
 	{
 		long sum = 0;
 		for(int element : elements)
@@ -49,7 +49,7 @@ public class MyMath
 		return sum;
 	}
 	
-	static long sum(List<Integer> elements)
+	public static long sum(List<Integer> elements)
 	{
 		long sum = 0;
 		for(Integer  element : elements)
@@ -60,26 +60,96 @@ public class MyMath
 	}
 	
 	
-	static int min(int a, int b)
+	public static int min(int a, int b)
 	{
 		return (a < b) ? a : b;
 	}
 	
-	static int max(int a, int b)
+	public static int max(int a, int b)
 	{
 		return (a > b) ? a : b;
 	}
 	
-	static long min(long a, long b)
+	public static long min(long a, long b)
 	{
 		return (a < b) ? a : b;
 	}
 	
-	static long max(long a, long b)
+	public static long max(long a, long b)
 	{	
 		return (a > b) ? a : b;
 	}
 	
+	public static int max(int[] elements)
+	{
+		int max = Integer.MAX_VALUE;
+		for(int elementValue : elements)
+		{
+			if(elementValue > max)
+				max = elementValue;
+		}
+		return max;
+	}
+	
+	
+	public static int count(int value, int[] elements)
+	{
+		int counter = 0;
+		for(int element : elements)
+		{
+			if (element == value)
+				counter++;
+		}
+		return counter;
+	}
+	
+	public static int count(double value, double[] elements)
+	{
+		int counter = 0;
+		for(double element : elements)
+		{
+			if (element == value)
+				counter++;
+		}
+		return counter;
+	}
+	
+	public static int countMax(int[] elements)
+	{
+		int counter = 0;
+		int max = Integer.MAX_VALUE; // autoboxing
+		
+		for(int element : elements)
+		{
+			if(element > max)
+			{
+				counter = 1;
+				max = element;
+			}
+			else if (element == max)
+				counter++;
+		}
+		return counter;
+		
+	}
+	
+	public static int countMin(int[] elements)
+	{
+		int counter = 0;
+		int min = Integer.MAX_VALUE; 
+		
+		for(int element : elements)
+		{
+			if(element < min)
+			{
+				counter = 1;
+				min = element;
+			}
+			else if (element == min)
+				counter++;
+		}
+		return counter;
+	}
 	
 	
 	
